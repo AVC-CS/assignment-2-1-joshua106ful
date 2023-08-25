5080ef099514f0d3aa6228a84a9b4675f20a4ba3
Joshua Williams <yourw20@gmail.comm>
2023-08-25 16:26:26 -0700

    Assignment 2-1 completed

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+    m_perc = int(input('Number of males:'))
+    f_perc = int(input('Number of females:'))
+    total = m_perc + f_perc 
+    
+    print(f"The total number of students: {total}")
+    print(f"The number of males and females: {m_perc} {f_perc} ")
+    print (f"The percentage of males and females: \t {m_perc:.2f}% {f_perc:.2f}%")
 
     """
     ########################################
     # Do not delete the return statement
     ########################################
     """
-    return m_perc, f_perc
+    return m_perc, f_perc, total
 
 
 if __name__ == '__main__':
