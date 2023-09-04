6378a0744de4b2b443f2487717e92e22bc2a2ee0
Kyuwoong Lee <kwooler@gmail.com>
2023-09-03 22:46:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def main():
     m_perc = int(input('Number of males:'))
     f_perc = int(input('Number of females:'))
     total = m_perc + f_perc 
+
+    """
+    You should make an expression to calculate the percentage
+    m_perc  = m_perc / total * 100
+    f_perc  = f_perc / total * 100
+    """
+    
+
     
     print(f"The total number of students: {total}")
     print(f"The number of males and females: {m_perc} {f_perc} ")
